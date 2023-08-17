2970d86c0502c71ec612e6e72885b376eebd2273
yeosf16 <141291333+yeosf16@users.noreply.github.com>
2023-08-17 10:59:47 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'RTD Test'
+copyright = '2021, Yeo'
+author = 'Yeo'
 
 release = '0.1'
 version = '0.1.0'
